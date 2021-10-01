2670a270498a4b94da6e9095a1bd43dd1725b01a
jakelheknight <jakelheknight@gmail.com>
2021-10-01 11:01:54 -0600

    adding in results

--- src/pages/SearchActors/index.js
+++ src/pages/SearchActors/index.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { useCleanTxt } from "../../hooks/useCleanTxt";
+import Results from "../../sharedComponents/Results/Results";
 
 export default function SearchActors() {
     const [firstName, setFirstName] = useCleanTxt();
     const [lastName, setLastName] = useCleanTxt();
     return (
+        <>
         <div>
             <div>
                 First Name: <input placeholder="first name" value={firstName} onChange={(e) => setFirstName(e?.target?.value)} />
@@ -13,5 +15,7 @@ export default function SearchActors() {
                 Last Name: <input placeholder="last name" value={lastName} onChange={(e) => setLastName(e?.target?.value)} />
             </div>
         </div>
+        <Results firstName={firstName} lastName={lastName}/>
+        </>
     )
 }
---
+++ src/sharedComponents/Paginator/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Paginator() {
+    return (
+        <div>
+            <button> prevous </button> {page} of {pages} <button> next </button> 
+        </div>
+    )
+}
